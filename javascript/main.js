d171190358d4d10519007a7fbb4a1975ca4011be
fatmamath <massoufatima23@gmail.com>
2024-01-07 17:59:20 +0100

    First Web Site With html/css/javaScript .

---
+++ javascript/main.js
@@ -0,0 +1,22 @@
+var menu = document.getElementById('menu')
+var navbar = document.querySelector('.header__menu')
+var close = document.querySelector('.close')
+
+menu.onclick = () => {
+  navbar.classList.toggle('active-menu')
+}
+
+close.onclick = () => {
+  navbar.classList.toggle('active-menu')
+}
+
+const cards = document.querySelectorAll('.popular-foods__card')
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    cards.forEach((card1) => {
+      card1.classList.remove('active-card')
+    })
+    card.classList.add('active-card')
+  })
+})
